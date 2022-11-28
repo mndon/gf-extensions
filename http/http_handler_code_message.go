6e1211f96fc62dacd580fd2f36bab2914fb2f800
yl3594 <linmd@yealink.com>
2022-11-28 14:39:06 +0800

    [fix] 1.规范code和错误处理

---
+++ http/http_handler_code_message.go
@@ -0,0 +1,34 @@
+package http
+
+import (
+	"context"
+	"github.com/mndon/gf-extensions/config"
+)
+
+const (
+	zh = "zh-CN"
+	en = "en"
+)
+
+var local string
+
+func Init() {
+	local = config.GetValueFromConfigWithPanic(context.TODO(), "local", en).String()
+}
+
+type I18nMsg struct {
+	Zh string
+	En string
+}
+
+func (i *I18nMsg) GetMsg() string {
+	if local == "" {
+		Init()
+	}
+
+	if local == zh {
+		return i.Zh
+	} else {
+		return i.En
+	}
+}
