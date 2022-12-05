0a6de35a6a7a7133d1f7fe38aa39c6fd1025a7cf
yl3594 <linmd@yealink.com>
2022-12-05 17:49:50 +0800

    [fix] 1.优化请求打印

--- http/http_middleware_log_request.go
+++ http/http_middleware_log_request.go
@@ -5,18 +5,34 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/util/gconv"
-	"strings"
 )
+
+const logFormat = `
+=================[API_CALL]=================
+uri : %s
+url : %s
+method : %s
+body : %s
+ip : %s
+token : %s
+userId : %s
+userAgent : %s 
+=================[/API_CALL]=================
+`
 
 // MiddlewareLogRequest 验证请求签名
 func MiddlewareLogRequest(r *ghttp.Request) {
-	uId := gconv.String(JwtAuth().GetIdentity(r.GetCtx()))
-	ua := strings.Join(r.Header[HeaderUA], "; ")
-	ip := GetRemoteIpFromCtx(r.GetCtx())
-	method := r.Method
-	url := r.URL.String()
+	ctx := r.GetCtx()
 	uri := r.Router.Uri
-	logStr := fmt.Sprintf("[MARK] [%s] [%s] [%s] [%s] [%s] [%s]", ip, method, url, uri, uId, ua)
-	g.Log().Info(r.GetCtx(), logStr)
+	url := r.URL.String()
+	method := r.Method
+	body := r.GetBodyString()
+	ip := GetRemoteIpFromCtx(ctx)
+	uId := gconv.String(JwtAuth().GetIdentity(ctx))
+	token := r.Header.Get(HeaderAuthorization)
+	ua := GetUaFromCtx(ctx)
+
+	logStr := fmt.Sprintf(logFormat, uri, url, method, body, ip, token, uId, ua)
+	g.Log().Info(ctx, logStr)
 	r.Middleware.Next()
 }
