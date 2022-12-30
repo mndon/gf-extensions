a1832d01b444feb702693fb5f703e8c8fc17a8b4
yl3594 <linmd@yealink.com>
2022-12-30 17:46:44 +0800

    [feat] 1.jwt oss相关优化

--- config/config_utils.go
+++ config/config_utils.go
@@ -6,8 +6,8 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 )
 
-func GetValueFromConfigWithPanic(ctx context.Context, key string, def ...interface{}) *g.Var {
-	value, err := g.Cfg().Get(ctx, key, def...)
+func GetWithCmdFromCfgWithPanic(ctx context.Context, key string, def ...interface{}) *g.Var {
+	value, err := g.Cfg().GetWithCmd(ctx, key, def...)
 	if err != nil {
 		g.Log().Errorf(ctx, fmt.Sprintf("get %s from config error", key))
 		panic(err)
