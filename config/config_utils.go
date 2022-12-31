0d14f3cd6ff9011936999da80af5283b89997894
mindonmindon <mindonmindon@qq.com>
2022-12-31 23:57:58 +0800

    [fix] 获取配置先从cmd中获取在从配置文件中获取

--- config/config_utils.go
+++ config/config_utils.go
@@ -3,14 +3,33 @@ package config
 import (
 	"context"
 	"fmt"
+	"github.com/gogf/gf/v2/container/gvar"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gcmd"
 )
 
+// GetWithCmdFromCfgWithPanic
+// @Description: 获取配置， 先从cmd获取， 获取不到再到配置文件获取
+// @param ctx
+// @param key
+// @param def
+// @return *g.Var
 func GetWithCmdFromCfgWithPanic(ctx context.Context, key string, def ...interface{}) *g.Var {
-	value, err := g.Cfg().GetWithCmd(ctx, key, def...)
+	// 先从cmd获取
+	if v := gcmd.GetOpt(key); v != nil {
+		return gvar.New(v)
+	}
+	// 再从配置文件获取
+	value, err := g.Cfg().Get(ctx, key)
 	if err != nil {
 		g.Log().Errorf(ctx, fmt.Sprintf("get %s from config error", key))
 		panic(err)
+	}
+	if value == nil {
+		if len(def) > 0 {
+			return gvar.New(def[0])
+		}
+		return nil
 	}
 	return value
 }
