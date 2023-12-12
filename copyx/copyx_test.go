face65215aa532cf32e41f66cf70a9a3c67df6f9
mindon <mindon@tttci.com>
2023-12-13 01:52:16 +0800

    [pref] 1.优化复制

--- copyx/copyx_test.go
+++ copyx/copyx_test.go
@@ -31,79 +31,8 @@ func TestCopyx(t *testing.T) {
 	}
 
 	t.Run("*struct to *struct", func(t *testing.T) {
-		sb := SB{}
-		err := Copy(&sa, &sb)
-		if err != nil {
-			t.Error(err)
-		}
-		t.Log("result sb: ", sb)
-	})
+		sb := Copy(sa, new(SB))
 
-	t.Run("*struct to *struct, ignore IgnoreEmpty", func(t *testing.T) {
-		sb := SB{
-			A: "BBBBB",
-			B: 1,
-			C: &Item{},
-			D: []int{1},
-		}
-		err := Copy(&sa, &sb)
-		if err != nil {
-			t.Error(err)
-		}
-		t.Log("result sb: ", sb)
-	})
-
-	t.Run("*struct to **struct", func(t *testing.T) {
-		var sb *SB
-		err := Copy(&sa, &sb)
-		if err != nil {
-			t.Error(err)
-		}
-		t.Log("result sb: ", sb)
-	})
-
-	t.Run("*struct to **struct, ignore IgnoreEmpty", func(t *testing.T) {
-		sb := &SB{
-			A: "BBBBB",
-			B: 1,
-			C: &Item{},
-			D: []int{1},
-		}
-		err := Copy(&sa, &sb)
-		if err != nil {
-			t.Error(err)
-		}
-		t.Log("result sb: ", sb)
-	})
-
-	t.Run("*struct to *struct, filed differences", func(t *testing.T) {
-		type SAA struct {
-			A *string
-			B *int
-			C *Item
-			D *[]int
-		}
-
-		saaA := "xxxxx"
-		saaD := []int{1, 2}
-
-		saa := SAA{
-			A: &saaA,
-			B: nil,
-			C: nil,
-			D: &saaD,
-		}
-
-		sb := &SB{
-			A: "BBBBB",
-			B: 1,
-			C: &Item{},
-			D: []int{1},
-		}
-		err := Copy(&saa, &sb)
-		if err != nil {
-			t.Error(err)
-		}
 		t.Log("result sb: ", sb)
 	})
 }
