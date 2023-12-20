759e0ea9515121428d07352936bbcdc6b85506f9
mindon <mindonmindon@qq.com>
2023-12-21 00:57:37 +0800

    [pref] 1.优化复制

--- copyx/copyx_test.go
+++ copyx/copyx_test.go
@@ -5,33 +5,34 @@ import (
 )
 
 func TestCopyx(t *testing.T) {
-	type Item struct {
-		sub string
-	}
 
 	type SA struct {
 		A string
-		B int
-		C *Item
-		D []int
 	}
 
 	type SB struct {
 		A string
-		B int
-		C *Item
-		D []int
-	}
-
-	sa := SA{
-		A: "q",
-		B: 0,
-		C: nil,
-		D: nil,
 	}
 
 	t.Run("*struct to *struct", func(t *testing.T) {
-		sb := Copy(sa, new(SB))
+		var a *SA
+		var b *SB
+		sb := Copy(a, b)
+
+		t.Log("result sb: ", sb)
+	})
+
+	t.Run("*struct to *struct", func(t *testing.T) {
+		a := &SA{A: "xxxxxxxxxxx"}
+		sb := Copy(a, new(SB))
+
+		t.Log("result sb: ", sb)
+	})
+
+	t.Run("*struct to *struct", func(t *testing.T) {
+		a := &SA{A: "xxxxxxxxxxx"}
+		b := &SB{}
+		sb := Copy(a, b)
 
 		t.Log("result sb: ", sb)
 	})
