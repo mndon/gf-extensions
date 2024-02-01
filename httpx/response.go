b37026dc052e490342f5d346dcdcd1f7989f614b
mindon <mindonmindon@qq.com>
2024-02-02 02:13:35 +0800

    [feat] 1.新增设置响应体remark方法

--- httpx/response.go
+++ httpx/response.go
@@ -1,5 +1,14 @@
 package httpx
 
+import (
+	"context"
+	"github.com/gogf/gf/v2/net/ghttp"
+)
+
+const (
+	ctxKeyForRemark = "__internal_http_response_remark"
+)
+
 // HandlerResponse
 // @Description: 响应结构体
 type HandlerResponse struct {
@@ -7,4 +16,20 @@ type HandlerResponse struct {
 	Msg    string      `json:"msg" dc:"Error message"`
 	Remark string      `json:"remark" dc:"client tip message"`
 	Data   interface{} `json:"data"    dc:"Result data for certain request according API definition"`
+}
+
+// GetResponseRemark
+// @Description: 获取响应体体提示内容
+// @param ctx
+// @return string
+func GetResponseRemark(ctx context.Context) string {
+	return ghttp.RequestFromCtx(ctx).GetCtxVar(ctxKeyForRemark).String()
+}
+
+// SetResponseRemark
+// @Description: 设置响应体提示内容
+// @param ctx
+// @param remark
+func SetResponseRemark(ctx context.Context, remark string) {
+	ghttp.RequestFromCtx(ctx).SetCtxVar(ctxKeyForRemark, remark)
 }
