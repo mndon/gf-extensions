eccf9b6e9a56f097568f3c5cd0b813e0726325e3
mindon <mindonmindon@qq.com>
2024-05-28 00:53:07 +0800

    [feat] 1.新增BuildErrf、BuildErrfWithCode

--- errorx/error.go
+++ errorx/error.go
@@ -1,11 +1,14 @@
 package errorx
 
 import (
+	"fmt"
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
 )
 
 type ErrFunc func() error
+
+type ErrFuncf func(data ...string) error
 
 // NewErr 新增err
 func NewErr(code int, msg string, remark ...string) error {
@@ -50,11 +53,27 @@ func BuildErr(code int, msg string, remark ...string) ErrFunc {
 		return NewErr(code, msg, remark...)
 	}
 }
+
+func BuildErrf(code int, msg string, remark string) ErrFuncf {
+	return func(data ...string) error {
+		return NewErr(code, msg, fmt.Sprintf(remark, data))
+	}
+}
+
 func BuildErrWithCode(code gcode.Code, msg string, remark ...string) ErrFunc {
 	return func() error {
 		if len(remark) == 0 {
 			return gerror.NewCode(code, msg)
 		}
 		return gerror.NewCode(gcode.New(code.Code(), remark[0], nil), msg)
+	}
+}
+
+func BuildErrfWithCode(code gcode.Code, msg string, remark string) ErrFuncf {
+	return func(data ...string) error {
+		if remark == "" {
+			return gerror.NewCode(code, msg)
+		}
+		return gerror.NewCode(gcode.New(code.Code(), fmt.Sprintf(remark, data), nil), msg)
 	}
 }
