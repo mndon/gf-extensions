5dff101821d2fc2cea2eb0ae4fab3a22cab4dd93
mindon <mindon@tttci.com>
2026-04-12 23:57:16 +0800

    [feat] 优化用户uid获取

--- httpx/header.go
+++ httpx/header.go
@@ -3,9 +3,11 @@ package httpx
 import (
 	"context"
 	"github.com/gogf/gf/v2/net/ghttp"
+	"strings"
 )
 
 const (
+	HeaderForwardedFor  = "x-forwarded-for"
 	HeaderRemoteIp      = "x-real-ip"
 	HeaderXA            = "x-agent"
 	HeaderUA            = "user-agent"
@@ -15,7 +17,14 @@ const (
 func GetRemoteIpFromCtx(ctx context.Context) string {
 	r := ghttp.RequestFromCtx(ctx)
 	if r != nil {
-		return r.Header.Get(HeaderRemoteIp)
+		ip := r.Header.Get(HeaderForwardedFor)
+		if ip == "" {
+			ip = r.Header.Get(HeaderRemoteIp)
+		}
+		if idx := strings.Index(ip, ","); idx >= 0 {
+			ip = ip[:idx]
+		}
+		return strings.TrimSpace(ip)
 	}
 	return ""
 }
